e5b46cfc97d0830f7ca09310a7e75ea9e853ce09
taek <taekgyu0602@gmail.com>
2020-12-30 13:21:39 +0900

    스레드 테스트1,2  isBackground

---
+++ WindowsFormsApp_Test1/Thread_Test1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(threadFunc);
+            t.IsBackground = true;  // 스레드가 실행 종료에 영향을 미치지 않도록 (프로그램은 호출당한 스레드의 실행 여부와 상관없이 종료된다.)
+            t.Start();
+
+            //t.Join();   // 인스턴스(t)가 나타내는 스레드가 종료될 때 까지 호출 스레드를 차단한다
+                        // ( t 스레드가 종료할 때까지 현재 스레드를 무한 대기 )
+            Console.WriteLine("주 스레드 종료");
+        }
+
+        static void threadFunc()
+        {
+            /* 주 스레드에서 스레드 객체의 Start 메서드를 실행했는데 threadFunc에 있는 코드가 실행이 안되는 이유는
+                스레드가 CPU에 의해 선택되어 실행될 수 있는 단계까지 시간이 걸리기 때문이다.
+                즉, threadFunc을 실행해야할 스레드가 운영체제의 스케줄러에 의해 선택되기도 전에
+                Main 메서드를 실행하는 스레드가 종료됐으므로 그런 현상이 발생한 것이다.
+            */
+            // 이를 해결하기 위해 Join 메서드를 사용하면 된다.
+            Console.WriteLine("3초 후 프로그램 종료");
+            Thread.Sleep(1000 * 3);
+            Console.WriteLine("스레드 종료");
+        }
+    }
+}
---
+++ WindowsFormsApp_Test1/Thread_Test2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("입력한 숫자까지의 소수 개수 출력 (종료: 'x' + Enter)");
+
+            while(true)
+            {
+                Console.WriteLine("숫자를 입력하세요");
+                string userNumber = Console.ReadLine();
+
+                if(userNumber.Equals("x",StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    Console.WriteLine("프로그램 종료");
+                    break;
+                }
+
+                //CountPrimeNumbers(userNumber);  // CountPrimeNumbers을 아래 스레드로 실행
+                Thread t = new Thread(CountPrimeNumbers);
+                t.IsBackground = true; // 스레드가 실행 종료에 영향을 미치지 않도록 ( 프로그램은 호출당한 스레드의 실행 여부와 상관없이 종료된다.)
+                t.Start(userNumber);  
+            }
+        }
+
+        static void CountPrimeNumbers(object initialValue)
+        {
+            string value = (string)initialValue;
+
+            int primeCandidate = int.Parse(value);
+
+            int totalPrimes = 0;
+
+            for(int i = 2; i < primeCandidate; i++)
+            {
+                if (IsPrime(i) == true)
+                    totalPrimes++;
+            }
+
+            Console.WriteLine("숫자 {0}까지의 소수의 개수 : {1}",value, totalPrimes);
+        }
+
+        static bool IsPrime(int candidate)
+        {
+            if ((candidate & 1) == 0)
+                return candidate == 2;
+
+            for(int i = 3; (i*i) <= candidate; i += 2)
+            {
+                if ((candidate % i) == 0)
+                    return false;
+            }
+
+            return candidate != 1;
+        }
+    }
+}
